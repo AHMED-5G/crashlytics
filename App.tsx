5ea70f9bdd14862567f6de7419156ccea48f1947
ahmed-5g <ahmed_5g@yahoo.com>
2023-01-19 07:36:23 +0200

    working

--- App.tsx
+++ App.tsx
@@ -1,118 +1,61 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useEffect, useState} from 'react';
+import {View, StyleSheet} from 'react-native';
+import crashlytics from '@react-native-firebase/crashlytics';
+import MedButton from './src/components/mini/MedButton';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+type SignInForm = {
+  email: string;
+};
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+function onSignIn(form: SignInForm): void {
+  crashlytics().log('User signed in.');
+  crashlytics().setAttribute('key', 'value');
+  crashlytics().setAttribute('userEmail', form.email);
 }
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App() {
+  const [enableCrash, setEnableCrash] = useState(false);
+  useEffect(() => {
+    crashlytics().log('App mounted.');
+  }, []);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const crash = () => {
+    crashlytics().recordError(new Error('test Error'));
+    crashlytics().crash();
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <View>
+        <MedButton
+          title="Sign In"
+          borderRadius={10}
+          onPress={() => {
+            setEnableCrash(true);
+            onSignIn({email: 'a@a.com'});
+          }}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <MedButton
+          disabled={!enableCrash}
+          borderRadius={10}
+          color="#db4437"
+          title="Test Crash"
+          onPress={() => crash()}
+        />
+      </View>
+    </View>
   );
 }
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  buttonContainer: {
+    marginTop: 20,
   },
 });
-
-export default App;
